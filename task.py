367763f98c9360ac9da2f003766b386abdf3a933
Cecilia <cecilia.gaytc@gmail.com>
2022-08-02 20:47:40 -0500

    Cambio de variable

--- task.py
+++ task.py
@@ -4,8 +4,9 @@ print("Cecilia")
 num1=13
 num2=23
 
-suma = num1+num2
-print("El resultafo es: ",suma)
+sumas = num1+num2
+print("El resultafo es: ",sumas)
 
 nombre = input("Introduce tu nombre: ")
+
 print("¡Hola " + nombre + "!")
