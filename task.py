f0e6af8228ee95d1875a3cbf2ad9874ed5ce364e
Cecilia <cecilia.gaytc@gmail.com>
2022-08-02 20:42:45 -0500

    Prueba

---
+++ task.py
@@ -0,0 +1,11 @@
+print("Gaytan")
+print("Cecilia")
+
+num1=13
+num2=23
+
+suma = num1+num2
+print("El resultafo es: ",suma)
+
+nombre = input("Introduce tu nombre: ")
+print("¡Hola " + nombre + "!")
