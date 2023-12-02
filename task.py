a2253059ea36935348f4f1cac70fb52e8cda4ccd
Cecilia <cecilia.gaytc@gmail.com>
2023-12-01 21:06:02 -0600

    Aqui inicia curso de Python

--- task.py
+++ task.py
@@ -7,6 +7,21 @@ num2=23
 sumas = num1+num2
 print("El resultafo es: ",sumas)
 
-nombre = input("Introduce tu nombre: ")
+"""nombre = input("Introduce tu nombre: ")
+"""
+"""print("¡Hola " + nombre + "!")
+"""
+"""Ejercicio 7
+--Escribir un programa que pida al usuario su peso (en kg) y estatura (en metros), 
+--calcule el índice de masa corporal y lo almacene en una variable,
+-- y muestre por pantalla la frase Tu índice de masa corporal es
+-- <imc> donde <imc> es el índice de masa corporal calculado redondeado con dos decimales."""
 
-print("¡Hola " + nombre + "!")
+peso =input ("Ingrese su peso:  ")
+estatura = input("Ingresa tu estatura:  ")
+
+"""print("Ingresa tu peso")
+print("Ingresa tu estatura")"""
+imc= peso*estatura
+print("Tu  IMC es:", imc)
+
