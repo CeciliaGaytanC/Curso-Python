5bd26a5b686343324f51bae47b58a5e417491da3
Cecilia <cecilia.gaytc@gmail.com>
2023-12-01 21:09:14 -0600

    Aqui inicia curso de Python1

---
+++ test01.py
@@ -0,0 +1,14 @@
+print('Hello my name is:' + 'name')
+
+name = 'Cecilia'
+# print('Hello my name is:' + 'name')
+print(name); print('Adios')
+name
+
+# ......AAAA
+numero1=5
+numero2=5
+if numero1==numero2:
+    print('Son Iguales')
+else:
+    print('No son Iguales')
