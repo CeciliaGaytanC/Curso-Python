77f02f9e0f99ea764a783471dc7da1948963fed5
Cecilia <cecilia.gaytc@gmail.com>
2023-12-05 18:34:38 -0600

    Aqui inician las Listas

---
+++ Listas.py
@@ -0,0 +1,3 @@
+# Las listas  son el equivalente a los arrays, estructura de datos que guarda cant. de valores
+# Es como una variable que guarda un valor a diferencia que la lista tiene varios espacios para guardar varios valores
+
